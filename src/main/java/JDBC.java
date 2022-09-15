a1ee97ff958cc2a83ca3572199f17da974a150fd
Beknazar460 <beknazarabdykalykuulu@gmail.com>
2022-09-16 02:03:21 +0600

    changed some code in somewhere

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -2,12 +2,15 @@ import java.sql.*;
 
 public class JDBC extends Configs{
     static Connection dbConnection;
+    static Statement dbStatment;
+    static ResultSet resultSet;
 
     public static void main(String[] args) {
-        signUpUser("1", "24", "Beknazar", "Abdykalykuulu", "beknazarabdykalykuulu@gmail.com", "+996555439317");
+        //signUpUser("1", "24", "Beknazar", "Abdykalykuulu", "beknazarabdykalykuulu@gmail.com", "+996555439317");
+        selectUser();
     }
     public static Connection getDbConnection() throws ClassNotFoundException, SQLException{
-        String connectionUrl = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+        String connectionUrl = "jdbc:mysql://localhost:3306/example";
 
         Class.forName("com.mysql.jdbc.Driver");
 
@@ -40,4 +43,29 @@ public class JDBC extends Configs{
 
     }
 
+    public static void selectUser() {
+        String select = "SELECT * FROM " + Const.USER_TABLE;
+
+        try {
+            dbStatment = getDbConnection().createStatement();
+            resultSet = dbStatment.executeQuery(select);
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                int age = resultSet.getInt(2);
+                String firstName = resultSet.getString(3);
+                String lastName = resultSet.getString(4);
+                String email = resultSet.getString(5);
+                String phone = resultSet.getString(6);
+
+                System.out.printf("%d, %d, %s, %s, %s, %s", id, age, firstName, lastName, email, phone);
+            }
+            resultSet.close();
+            dbStatment.close();
+            dbConnection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
